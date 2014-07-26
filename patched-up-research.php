212a58e3454521c10e52828e096afa82266c7c25
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-07-26 10:19:04 -0400

    Adds essay and argument post types along with admin menu

--- patched-up-research.php
+++ patched-up-research.php
@@ -11,8 +11,11 @@
 
 class Patched_Up_Research {
 	public static function init() {
+		add_action( 'init', 'Patched_Up_Research::register_essay_post_type' );
+		add_action( 'init', 'Patched_Up_Research::register_argument_post_type' );
 		add_action( 'init', 'Patched_Up_Research::register_note_post_type' );
 
+		add_action( 'admin_menu', 'Patched_Up_Research::create_admin_menu' );
 	}
 
 	public static function register_note_post_type() {
@@ -23,25 +26,47 @@ class Patched_Up_Research {
 					'name' => __( 'Notes' ),
 					'singular_name' => __( 'Note' ) ),
 				'menu_icon' => '',
+				'show_in_menu' => 'patched_up_research',
 				'public' => true,
 				'has_archive' => true
 			)
 		);
 	}
 
-}
+	public static function register_argument_post_type() {
+		register_post_type( 
+			'patched_up_argument', 
+			array( 
+				'labels' => array(
+					'name' => __( 'Arguments' ),
+					'singular_name' => __( 'Argument' ) ),
+				'menu_icon' => '',
+				'show_in_menu' => 'patched_up_research',
+				'public' => true,
+				'has_archive' => true
+			)
+		);
+	}
 
-function add_menu_icons_styles(){
-?>
- 
-<style>
-#adminmenu .menu-icon-patched_up_note div.wp-menu-image:before {
-  content: '\f119';
+	public static function register_essay_post_type() {
+		register_post_type( 
+			'patched_up_essay', 
+			array( 
+				'labels' => array(
+					'name' => __( 'Essays' ),
+					'singular_name' => __( 'Essay' ) ),
+				'menu_icon' => '',
+				'show_in_menu' => 'patched_up_research',
+				'public' => true,
+				'has_archive' => true
+			)
+		);
+	}
+
+	public static function create_admin_menu() {
+		add_menu_page( 'Research', 'Research', 'edit_posts', 'patched_up_research', '?post_type=patched_up_note', 'dashicons-welcome-write-blog', 26 );
+	}
+
 }
-</style>
- 
-<?php
-}
-add_action( 'admin_head', 'add_menu_icons_styles' );
 
 Patched_Up_Research::init();
